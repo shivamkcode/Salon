d99a89da20f07794ba829961d4be6bb88f81c3e7
shivamKcode <shivambaniya246@gmail.com>
2025-02-08 12:42:24 -0500

    fixed gallery page

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -9,8 +9,9 @@ const Gallery = () => {
   const gender = useContext(GenderContext);
 
   useEffect(() => {
-          document.title = gender === "male" ? "À-ONÈ | Gallery" : "ÀYÈSHÀ_ | Gallery";
-        }, [gender]);
+    document.title =
+      gender === "male" ? "À-ONÈ | Gallery" : "ÀYÈSHÀ_ | Gallery";
+  }, [gender]);
 
   return (
     <div
@@ -19,24 +20,30 @@ const Gallery = () => {
       } text-white sm:text-center p-6 md:p-8 lg:p-10 md:py-10 lg:py-16 flex flex-col gap-4 sm:gap-6 md:gap-8 lg:gap-10`}
     >
       <h1 className="text-5xl uppercase font-thin">Gallery</h1>
-      <p className="tracking-wider opacity-75 lg:w-[80vw] mx-auto">Taking the art of barbering to another level, {gender === "male" ? "À-ONÈ" : "ÀYÈSHÀ_"} presents you with a
-        collection of images from our world of style and elegance. Discover
-        moments captured in our barber chair, where haircuts become works of art
-        and every shave is an unforgettable experience. Browse our photo gallery
-        to feel the atmosphere and passion that define {gender === "male" ? "À-ONÈ" : "ÀYÈSHÀ_"}.</p>
+      <p className="tracking-wider opacity-75 lg:w-[80vw] mx-auto">
+        Taking the art of barbering to another level,{" "}
+        {gender === "male" ? "À-ONÈ" : "ÀYÈSHÀ_"} presents you with a collection
+        of images from our world of style and elegance. Discover moments
+        captured in our barber chair, where haircuts become works of art and
+        every shave is an unforgettable experience. Browse our photo gallery to
+        feel the atmosphere and passion that define{" "}
+        {gender === "male" ? "À-ONÈ" : "ÀYÈSHÀ_"}.
+      </p>
       <div
-        className={`flex ${
-          gender === "male" ? "flex-col" : "flex-col-reverse"
-        } contain-content sm:flex-row flex-wrap justify-evenly gap-4 lg:gap-8`}
+        className={`flex ${gender === 'male' ? 'flex-col' : 'flex-col-reverse'} gap-8`}
       >
-        {menImages.sort(() => Math.random() - 0.5).map((item) => (
-          <GalleryItem prop={item.url} key={item.id} />
-        ))}
-        {womenImages.sort(() => Math.random() - 0.5).map((item) => (
-          <GalleryItem prop={item.url} key={item.id} />
-        ))}
+        <div className="flex flex-1 contain-content flex-row flex-wrap justify-evenly gap-4 lg:gap-8">
+          {menImages.sort(() => Math.random() - 0.5).map((item) => (
+            <GalleryItem prop={item.url} key={item.id} />
+          ))}
+        </div>
+        <div className="flex flex-1 contain-content flex-row flex-wrap justify-evenly gap-4 lg:gap-8">
+          {womenImages.sort(() => Math.random() - 0.5).map((item) => (
+            <GalleryItem prop={item.url} key={item.id} />
+          ))}
+        </div>
       </div>
-      <InfoSection sex={gender}/>
+      <InfoSection sex={gender} />
     </div>
   );
 };
